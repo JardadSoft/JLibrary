d9ca333752d28511ed61778e4eae55e7e7f48bca
Saddam Boto <Saddam.Boto@outlook.com>
2023-09-12 21:14:25 +0300

    Initial commit

---
+++ JLibrary/src/main/java/com/JARDAD/JLlibrary/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.JARDAD.JLlibrary
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Toast.makeText(this, "- Demo Library -", Toast.LENGTH_SHORT).show()
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "JLibrary"
+include(":app")
+include(":JLibrary")
